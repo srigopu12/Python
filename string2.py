55fdca9b296b668866f7e9588ba2b915e34c4dc7
srilakshmi gopu <161108057+srigopu12@users.noreply.github.com>
2024-09-30 23:12:11 +0530

    Create string2.py

---
+++ string2.py
@@ -0,0 +1,21 @@
+##### concat in built function:
+name = "sri"
+name2 = "lakshmi"
+name3 = "Gopu"
+result = name+name2+name3
+print(result)
+######### uper case and lowercase
+name = "sri"
+test = name.upper()
+print("uppercase:" ,test)
+name = "sri"
+test = name.lower()
+print("lowercase:" ,test)
+#######length of the string
+test = "Python is a program language"
+result = len(text)
+print("length of the string:" ,result)
+##### replace the test
+text = "Python is awesome"
+new_text = text.replace("awesome", "great")
+print("Modified text:", new_text)
