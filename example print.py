84069b353da47f05dee87dadeaad27389adbffdd
srilakshmi gopu <161108057+srigopu12@users.noreply.github.com>
2024-09-30 22:19:36 +0530

    Create example print.py

---
+++ example print.py
@@ -0,0 +1,32 @@
+Name = "sri"
+Age = 25
+Hobby = "playing"
+# multiline 
+print("Name:" Name ,"Age:" Age)
+# f string
+print(f"{Name} is {Age} and {Hobby}")
+# % operator
+print("%s is %d years old." % (name, age))
+# sring format
+print("{} is {} years old.".format(name, age))
+#seprator
+print("Hello","world" sep=",")
+#end
+print("hello", end=",")
+print("world!")
+# get the deatils from file and change and read the file
+# Open the file in write mode
+with open("output.txt", "w") as f:
+    print("Hello, World!", file=f)
+    print("This is a second line.", file=f)
+    print("And this is the third line.", file=f)
+
+# Optionally, read back the contents to verify
+with open("output.txt", "r") as f:
+    contents = f.read()
+    print("Contents of the file:")
+    print(contents)
+
+
+
+
